05d304b1214909604670337e88e1226687583e79
Kyler Gibbs <kylergibbs@hotmail.com>
2024-07-17 17:40:04 -0700

    Error res.

--- client/src/Pages/Account.jsx
+++ client/src/Pages/Account.jsx
@@ -1,13 +1,28 @@
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_CURRENT } from '../utils/queries'; 
 import Auth from '../utils/auth';
+import {ADD_PRODUCT} from '../utils/mutations'
 
 function Account() {
 
   const { loading, data } = useQuery(QUERY_CURRENT);
+  const [addProduct, { error }] = useMutation(ADD_PRODUCT);
 
   const userData = data?.current || {};
     console.log(userData);
+
+    const productSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        const productRes = await addProduct({
+          variables: {
+            productName: formState.productName,
+            description: formState.description,
+            price: formState.price,
+            shippingAddress: formState.shippingAddress,
+          },
+        });
+      }
 
     return (
       <div>
@@ -68,8 +83,68 @@ function Account() {
         })}
       </div>
 
+
+{/* ///////////////////////////////////////////// */}
+
+<div className="mt-4 p-5 bg-dark text-white rounded">
+      <div className="mb-3">
+        <h2 className="">Create a product for sale</h2>
+        <form onSubmit={productSubmit}>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="productName">Name:</label>
+            <input className = "form-control"
+              placeholder="Product Name"
+              name="productName"
+              type="productName"
+              id="productName"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="description">Description:</label>
+            <input className = "form-control"
+              placeholder="Write a description of your product"
+              name="description"
+              type="description"
+              id="description"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="password">Price:</label>
+            <input className = "form-control"
+              placeholder="Your Username"
+              name="password"
+              type="password"
+              id="password"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="shipping">Shipping Address:</label>
+            <input className = "form-control"
+              placeholder="Shipping Address"
+              name="shippingAddress"
+              type="shippingAddress"
+              id="shipping"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <button className="btn btn-outline-light" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      <Link to="/login">Already have an account? Click here.</Link>
+    </div>
+
+{/* ///////////////////////////////////////////// */}
+
+
       </div>
     )
-};
-
+}
+}
 export default Account;
